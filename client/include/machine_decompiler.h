c70f0481983fd2aae4a517bec52014f2be040cc6
jha <24598074+jha@users.noreply.github.com>
2018-12-22 14:00:22 -0500

    Rename project owner

--- client/include/machine_decompiler.h
+++ client/include/machine_decompiler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 dreadiscool
+ * Copyright 2018 github.com/jha
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
