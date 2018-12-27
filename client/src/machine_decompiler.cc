0f3e1af49b27ab207af5b906c03d893520b6d6dd
jha <24598074+jha@users.noreply.github.com>
2018-12-27 15:23:21 -0500

    Open basic ImGui window

--- client/src/machine_decompiler.cc
+++ client/src/machine_decompiler.cc
@@ -24,16 +24,87 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
 #include "machine_decompiler.h"
 
 namespace machine_decompiler {
 namespace client {
 
-void MachineDecompiler::ShowWindow() {
-  
+namespace {
+
+void glfw_error_callback(int error, char const* description) {
+  fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-void MachineDecompiler::LoadBinary(std::string &path) {
+} // namespace
+
+void MachineDecompiler::ShowWindow() {
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit()) {
+    fprintf(stderr, "glfwInit()\n");
+    return;
+  }
+
+#if __APPLE__
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  auto const* glsl_version = "#version 150";
+#else // __APPLE__
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  auto const* glsl_version = "#version 130";
+#endif // __APPLE__
+
+  auto* window = glfwCreateWindow(1920, 1080, "Dear ImGui GLFW+OpenGL3",
+      nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  if (gl3wInit() != 0) {
+    fprintf(stderr, "gl3wInit()\n");
+    return;
+  }
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  auto& io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+  ImGui::StyleColorsDark();
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("Hello, world!");
+    ImGui::Text("This is some useful text.");
+    ImGui::End();
+
+    ImGui::Render();
+    glfwMakeContextCurrent(window);
+    int dw, dh;
+    glfwGetFramebufferSize(window, &dw, &dh);
+    glViewport(0, 0, dw, dh);
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwMakeContextCurrent(window);
+    glfwSwapBuffers(window);
+  }
+}
+
+void MachineDecompiler::LoadBinary(std::string& path) {
 
 }
 
